98492f76e247bc940de5ad5ca9dce7d6aba77185
RSNirwan <rajbir_nirwan@yahoo.com>
2017-12-27 16:58:40 +0100

    bug fix

--- src/displayWidgets/forms.py
+++ src/displayWidgets/forms.py
@@ -12,6 +12,6 @@ class UserForm(forms.ModelForm):
 		ascii_list = list(range(97,123)) + list(range(65,91)) + list(range(48,58))
 		user_name = self.cleaned_data.get('user_name')
 		for char in user_name:
-			if char not in ascii_list:
+			if int(char) not in ascii_list:
 				raise forms.ValidationError('Only following characters allowed: [a-z][A-Z][0-9]')
 		return user_name
